52895ee889b2cd4a84ea1e30ae45287a38b053a2
serj <seregaproger@yandex.ru>
2020-07-17 23:52:35 +0300

    test true

--- hero_spec.rb
+++ hero_spec.rb
@@ -2,4 +2,16 @@ require './hero'
 
 describe Hero do
   
+  it "has a capitalize name" do
+    hero = Hero.new 'mike'
+
+    expect(hero.name).to eq 'Mike'
+  end
+
+  it "can power up" do
+    hero = Hero.new 'mike'
+
+    expect(hero.power_up).to eq 110
+  end
+
 end
